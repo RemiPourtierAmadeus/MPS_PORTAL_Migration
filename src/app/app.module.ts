4ceeab2133b9bc54a2518cd2ee2219aab738df4d
RemiPourtierAmadeus <remi.pourtier@gmail.com>
2016-09-27 14:36:43 +0200

    Cleaning project + starting home page component integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import {ConnectionContentModule} from "./components/connection/connection-content/connection-content.module";
+import {HomeComponent} from './components/home-page/home/home.component';
 
 @NgModule({
   declarations: [
---
+++ src/app/components/home-page/home/home.module.ts
@@ -0,0 +1,3 @@
+/**
+ * Created by rpourtier on 27/09/2016.
+ */
