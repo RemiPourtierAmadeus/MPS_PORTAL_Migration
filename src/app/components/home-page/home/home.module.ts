80621ff91fb40cd5e3b66317857ef55199d7a57a
RemiPourtierAmadeus <remi.pourtier@gmail.com>
2016-09-27 17:09:06 +0200

    All bug fixed for news panel into homemodule

--- src/app/components/home-page/home/home.module.ts
+++ src/app/components/home-page/home/home.module.ts
@@ -3,12 +3,13 @@ import {MyCommonModule} from "../../../mycommon/mycommon.module";
 import { HomeComponent } from './home.component';
 import {NewsPanelComponent, PerformancePanelComponent, MetricsPanelComponent} from "../index";
 import {ManageNewsService} from "../../../shared/services/manage-news.service";
+import {NewsConstantService} from "../../../shared/services/news-constant.service";
 
 @NgModule({
   imports: [MyCommonModule],
-  declarations: [HomeComponent, MetricsPanelComponent, PerformancePanelComponent],
+  declarations: [HomeComponent, MetricsPanelComponent, PerformancePanelComponent, NewsPanelComponent],
   exports: [HomeComponent],
-  providers : [ManageNewsService],
+  providers : [ManageNewsService, NewsConstantService]
 })
 
 export class HomeModule { }
